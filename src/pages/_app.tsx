7685cdddc123af7910cff65caa6592f67723f43d
David <david-bourrel@hotmail.fr>
2023-02-22 19:17:00 +0100

    Init todo list

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+import '@/styles/globals.css';
+import Layout from '@/components/modules/Layout';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <div
+      id='app'
+      className='bg-white dark:bg-neutral-800 relative flex flex-col items-center w-full min-h-screen'
+    >
+      <ThemeProvider attribute='class'>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </div>
+  );
+};
+export default App;
